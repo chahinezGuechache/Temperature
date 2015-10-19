7d0685736f0d7418e5d72d6c6f0cbcb829c34edc
chahinezGuechache <chahineznouza@gmail.com>
2015-10-19 21:00:04 +0200

    Update Temperature.java

--- Temperature.java
+++ Temperature.java
@@ -28,7 +28,58 @@ public class Temperature {
 	double conversion(double nombre ){
 		return 9/5*nombre+32;
 	}
+
+
+
+// la  class fille 
+
+public class AfficheTemperature extends Temperature {
+	//constructeur 
+	AfficheTemperature(double nombre,char echelle){
+		super(nombre, echelle);
+	}
 	
+	public static void afficheEchelle(){
+		switch(echelle) {
+		  case 'c':
+			  System.out.println(+Temperature.getNombre()+"  Celsius");
+		    break;
+		  case 'f':
+			  System.out.println(+Temperature.getNombre()+"  Fahrenheit");
+		    break;
+		  case 'k':
+			  System.out.println(+Temperature.getNombre()+"  kelvin");
+		    break;
+			
+		
+			
+		
+		
+		
+		
+	}
+
+
+// le main qui les exploites
+
+public class EssayTemperature {
+
+	public static void main(String[] args) {
+		
+		
+		Temperature a=new Temperature(22,'c');
+		 System.out.println(+a.getNombre()+"  "+a.getEchelle());
+		 System.out.println(+a.conversion(+Temperature.getNombre())+"  Fahrenheit");
+ 
+		 AfficheTemperature b=new AfficheTemperature(22,'c');
+		
+		 b.afficheEchelle();
+	
+		 
+	}
+
+}
+
 
 	
 	
